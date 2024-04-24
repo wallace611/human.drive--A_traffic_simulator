556857ba29dfc055d95185efd996955a53558503
wallace611 <wallacef930814@gmail.com>
2024-04-24 14:45:12 +0800

    24.4.24 2:45 update
    add Vehiccle class
    add vscode environment

---
+++ src/com/HDEngine/Object/Dynamic/Vehicle.java
@@ -0,0 +1,11 @@
+package com.HDEngine.Object.Dynamic;
+
+public class Vehicle extends DynamicObj{
+    private double maxSpeed;
+    private double acceleration;
+    private double reactionDelay;
+
+    public Vehicle() {
+        super();
+    }
+}
--- src/com/HDEngine/Object/HDObject.java
+++ src/com/HDEngine/Object/HDObject.java
@@ -3,5 +3,19 @@ package com.HDEngine.Object;
 import com.HDEngine.Utilities.*;
 
 public class HDObject {
-    Vector2D location;
+    private Vector2D location;
+    private double rotation;
+
+    public HDObject() {
+        location = new Vector2D();
+        rotation = 0.0f;
+    }
+
+    public void begin() {
+
+    }
+
+    public void tick(double deltaTime) {
+
+    }
 }
