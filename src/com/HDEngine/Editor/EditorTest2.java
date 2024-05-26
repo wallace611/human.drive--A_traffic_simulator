0a1da4e707ad55634606bef15ae45a3e081b6565
JackChin-txt <uniamlajack@gmail.com>
2024-05-26 18:13:35 +0800

    24.05.26 16:57
    update Exception handling
    
    UPDATED:
    1.most of the exception
    
    2.weight update to double[]
    
    3.connection judge & setWeight update
    
    TODO
    待討論

--- src/com/HDEngine/Editor/EditorTest2.java
+++ src/com/HDEngine/Editor/EditorTest2.java
@@ -7,7 +7,8 @@ public class EditorTest2 {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        int ID_X = 0, ID_Y = 0;
+        int ID_X = 0, ID_Y = 0, runset=0;
+        runset = input.nextInt();
         Editor editor = new Editor();
         FileManager loadedFileManager = FileManager.loadFromFile("D:\\coding\\java\\Project\\human.drive--A_traffic_simulator\\src\\com\\HDEngine\\Utilities\\SavedFile\\editor_map.obj");
         if (loadedFileManager != null) 
@@ -16,9 +17,12 @@ public class EditorTest2 {
             System.out.println("Map loaded!!!!!!");
         }
         System.out.println("EditorTest start");
-        ID_X = input.nextInt();
-        ID_Y = input.nextInt();
-        System.out.println(editor.getChunk(ID_X, ID_Y));
+        for(int i = 0 ; i < runset ; i++)
+        {
+            ID_X = input.nextInt();
+            ID_Y = input.nextInt();
+            System.out.println(editor.getChunk(ID_X, ID_Y));
+        }
         input.close();
     }
 }
